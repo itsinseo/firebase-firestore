c3c92ded09e701343c92bb82adcd98ae0a836780
Inseo Kim <itsinseo@gmail.com>
2024-06-03 17:03:46 +0900

    git init

---
+++ src/main/java/org/example/firebasefirestore/FirebaseFirestoreApplication.java
@@ -0,0 +1,13 @@
+package org.example.firebasefirestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirebaseFirestoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirebaseFirestoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/firebasefirestore/FirebaseFirestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.firebasefirestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirebaseFirestoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
